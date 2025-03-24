a4f0c6ff6b019b1ff60d4b1bee292843575d6740
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2025-03-24 07:53:03 -0300

    finalizando aula 45 sobre filter

---
+++ Aula45_Arrays_Avancado/04_filter.js
@@ -0,0 +1,7 @@
+// Filter vai filtrar um valor do array
+
+// Retornar os números maior que 10
+const numeros = [5, 50, 80, 2, 1, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const maiorquedez = numeros.filter((n) => n > 10)
+console.log(maiorquedez)
