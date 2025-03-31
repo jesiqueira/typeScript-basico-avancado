a1ddb3c348530fef9e9aff958317df9930857dfa
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2025-03-30 22:58:38 -0300

    Finalizado  estudo de module import/export

---
+++ Aula51_module_import_export/src/index.js
@@ -0,0 +1,4 @@
+import { nome, sobrenome, soma } from './modulo1'
+console.log(nome)
+console.log(sobrenome)
+console.log(soma(4, 4))
---
+++ Aula51_module_import_export/src/modulo1.js
@@ -0,0 +1,8 @@
+export const nome = 'Edmar'
+export const sobrenome = 'Siqueira'
+
+export const soma = (a, b) => {
+  return a + b
+}
+
+// export { nome, sobrenome, soma }
