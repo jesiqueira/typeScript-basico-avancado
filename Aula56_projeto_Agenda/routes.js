37ad5cfbce33dc92dcd8f61bb0c381e0f2876eef
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2025-04-22 22:06:31 -0300

    Configurado mongoDB

--- Aula56_projeto_Agenda/routes.js
+++ Aula56_projeto_Agenda/routes.js
@@ -6,6 +6,8 @@ const controlerContato = require('./src/controllers/contato')
 
 // rotas da home
 route.get('/', controlerHome.home)
+
+// rotas do contato
 route.post('/contato', controlerContato.contato)
 
 module.exports = route
--- Aula56_projeto_Agenda/src/controllers/home/index.js
+++ Aula56_projeto_Agenda/src/controllers/home/index.js
@@ -1,10 +1,3 @@
 exports.home = (req, res) => {
   res.render('index')
-  // res.send(`
-  //   <form action="/contato" method="POST">
-  //     <input type="text" name="nome" placeholder="Nome">
-  //     <input type="text" name="email" placeholder="E-mail">
-  //     <button>Enviar</button>
-  //   </form>
-  //   `)
 }
